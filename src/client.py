de3ff02e6f2b58b9085ad8b79cc0ed1c8d156306
Leon Liu <w110056005@mail.nchu.edu.tw>
2023-08-23 21:58:20 +0800

    Revert "change client method"
    This reverts commit e25932bbb4b1703f16b64e34455a3d68eba1b5bc.

--- src/client.py
+++ src/client.py
@@ -1,47 +1,27 @@
-import warnings
 import flwr as fl
-import numpy as np
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import log_loss
+import tensorflow as tf
 import sys
-import utils
 
 port = sys.argv[1]
 
-if __name__ == "__main__":
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-    (X_train, y_train), (X_test, y_test) = utils.load_mnist()
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 
-    partition_id = np.random.choice(10)
-    (X_train, y_train) = utils.partition(X_train, y_train, 10)[partition_id]
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
 
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
+        return model.get_weights(), len(x_train), {}
 
-model = LogisticRegression(
-    penalty="l2",
-    max_iter=1,  # local epoch
-    warm_start=True,  # prevent refreshing weights when fitting
-)
-
-utils.set_initial_params(model)
-
-class MnistClient(fl.client.NumPyClient):
-    def get_parameters(self, config):  # type: ignore
-        return utils.get_model_parameters(model)
-
-    def fit(self, parameters, config):  # type: ignore
-        utils.set_model_params(model, parameters)
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            model.fit(X_train, y_train)
-        print(f"Training finished for round {config['server_round']}")
-        return utils.get_model_parameters(model), len(X_train), {}
-
-    def evaluate(self, parameters, config):  # type: ignore
-        utils.set_model_params(model, parameters)
-        loss = log_loss(y_test, model.predict_proba(X_test))
-        accuracy = model.score(X_test, y_test)
-        return loss, len(X_test), {"accuracy": accuracy}
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": float(accuracy)}
 
 addr = "sower_platform_container:"+port
-fl.client.start_numpy_client(server_address=addr, client=MnistClient())
+fl.client.start_numpy_client(server_address=addr, client=CifarClient())
